27660690c19282270b0025c27136e04cfdbb831f
Dipesh Poudel <43557135+Dpoudel18@users.noreply.github.com>
2021-09-14 22:05:21 -0400

    color change

--- lendingClub_dashboard.py
+++ lendingClub_dashboard.py
@@ -331,7 +331,7 @@ if page == "Employment Duration and Loan Type":
     unsafe_allow_html=True,
     )
     #st.table(emp_length_bad_loan_df.assign(hack='').set_index('hack'))
-    fig4 = plot_line_chart(emp_length_bad_loan_df, 'emp_length_years', 'count','Bad Loan and Employment Duration','maroon')    
+    fig4 = plot_line_chart(emp_length_bad_loan_df, 'emp_length_years', 'count','Bad Loan and Employment Duration','pink')    
     st.plotly_chart(fig4)
     st.markdown("<u>Observations:</u>",unsafe_allow_html=True)
     st.markdown("- The lowest percentage of 'Good Loans' or the highest percentage of 'Bad Loans' exist in the category of missing values where people don't report their employment length. It is highly likely they don't have a job to pay their loans on time.") 
